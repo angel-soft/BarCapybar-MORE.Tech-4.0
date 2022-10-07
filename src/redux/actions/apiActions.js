2ae8afcee3c1f662f2e57ca6710691460e317b29
mrcheater <aa_zhukov@controlsystems.ru>
2022-10-07 20:48:57 +0300

    WIP

---
+++ src/redux/actions/apiActions.js
@@ -0,0 +1,163 @@
+import {
+    TRANSFER_MATIC_REQUEST,
+    TRANSFER_MATIC_SUCCESS,
+
+    TRANSFER_RUBLE_REQUEST,
+    TRANSFER_RUBLE_SUCCESS,
+
+    TRANSFER_NFT_REQUEST,
+    TRANSFER_NFT_SUCCESS,
+
+    TRANSFER_STATUS_REQUEST,
+    TRANSFER_STATUS_SUCCESS,
+
+    WALLET_BALANCE_REQUEST,
+    WALLET_BALANCE_SUCCESS,
+
+    WALLET_NFT_BALANCE_REQUEST,
+    WALLET_NFT_BALANCE_SUCCESS,
+
+    GENERATE_NFT_REQUEST,
+    GENERATE_NFT_SUCCESS,
+
+    LIST_NFT_REQUEST,
+    LIST_NFT_SUCCESS,
+
+    STATUS_NFT_REQUEST,
+    STATUS_NFT_SUCCESS,
+
+    WALLET_HISTORY_REQUEST,
+    WALLET_HISTORY_SUCCESS,
+} from "../actionTypes"
+
+export const transferMaticRequest = () => ({
+    type: TRANSFER_MATIC_REQUEST,
+    payload: {
+
+    }
+})
+export const transferMaticSuccess = () => ({
+    type: TRANSFER_MATIC_SUCCESS,
+    payload: {
+
+    }
+})
+
+export const transferRubleRequest = () => ({
+    type: TRANSFER_RUBLE_REQUEST,
+    payload: {
+
+    }
+})
+export const transferRubleSuccess = () => ({
+    type: TRANSFER_RUBLE_SUCCESS,
+    payload: {
+
+    }
+})
+
+export const transferNftRequest = () => ({
+    type: TRANSFER_NFT_REQUEST,
+    payload: {
+
+    }
+})
+export const transferNftSuccess = () => ({
+    type: TRANSFER_NFT_SUCCESS,
+    payload: {
+
+    }
+})
+
+export const transferStatusRequest = () => ({
+    type: TRANSFER_STATUS_REQUEST,
+    payload: {
+
+    }
+})
+export const transferStatusSuccess = () => ({
+    type: TRANSFER_STATUS_SUCCESS,
+    payload: {
+
+    }
+})
+
+export const walletBalanceRequest = () => ({
+    type: WALLET_BALANCE_REQUEST,
+    payload: {
+
+    }
+})
+export const walletBalanceSuccess = () => ({
+    type: WALLET_BALANCE_SUCCESS,
+    payload: {
+
+    }
+})
+
+export const walletNftBalanceRequest = () => ({
+    type: WALLET_NFT_BALANCE_REQUEST,
+    payload: {
+
+    }
+})
+export const walletNftBalanceSuccess = () => ({
+    type: WALLET_NFT_BALANCE_SUCCESS,
+    payload: {
+
+    }
+})
+
+export const generateNftRequest = () => ({
+    type: GENERATE_NFT_REQUEST,
+    payload: {
+
+    }
+})
+export const generateNftSuccess = () => ({
+    type: GENERATE_NFT_SUCCESS,
+    payload: {
+
+    }
+})
+
+export const listNftRequest = () => ({
+    type: LIST_NFT_REQUEST,
+    payload: {
+
+    }
+})
+export const listNftSuccess = () => ({
+    type: LIST_NFT_SUCCESS,
+    payload: {
+
+    }
+})
+
+export const statusNftRequest = () => ({
+    type: STATUS_NFT_REQUEST,
+    payload: {
+
+    }
+})
+export const statusNftSuccess = () => ({
+    type: STATUS_NFT_SUCCESS,
+    payload: {
+
+    }
+})
+
+export const walletHistoryRequest = () => ({
+    type: WALLET_HISTORY_REQUEST,
+    payload: {
+
+    }
+})
+export const walletHistorySuccess = () => ({
+    type: WALLET_HISTORY_SUCCESS,
+    payload: {
+
+    }
+})
+
+
