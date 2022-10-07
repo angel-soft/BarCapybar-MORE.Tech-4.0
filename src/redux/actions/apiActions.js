f05c81c6c5b607d7e6c6e00f709a9c94aaf202e7
mrcheater <aa_zhukov@controlsystems.ru>
2022-10-07 21:02:06 +0300

    WIP

--- src/redux/actions/apiActions.js
+++ src/redux/actions/apiActions.js
@@ -30,133 +30,160 @@ import {
     WALLET_HISTORY_SUCCESS,
 } from "../actionTypes"
 
-export const transferMaticRequest = () => ({
+export const transferMaticRequest = ({ fromPrivateKey, toPublicKey, amount}) => ({
     type: TRANSFER_MATIC_REQUEST,
     payload: {
-
+        fromPrivateKey,
+        toPublicKey,
+        amount
     }
 })
-export const transferMaticSuccess = () => ({
+export const transferMaticSuccess = ({ fromPrivateKey, toPublicKey, amount, transactionHash }) => ({
     type: TRANSFER_MATIC_SUCCESS,
     payload: {
-
+        fromPrivateKey,
+        toPublicKey,
+        amount,
+        transactionHash
     }
 })
 
-export const transferRubleRequest = () => ({
+export const transferRubleRequest = ({ fromPrivateKey, toPublicKey, amount }) => ({
     type: TRANSFER_RUBLE_REQUEST,
     payload: {
-
+        fromPrivateKey,
+        toPublicKey,
+        amount
     }
 })
-export const transferRubleSuccess = () => ({
+export const transferRubleSuccess = ({ fromPrivateKey, toPublicKey, amount, transactionHash}) => ({
     type: TRANSFER_RUBLE_SUCCESS,
     payload: {
-
+        fromPrivateKey,
+        toPublicKey,
+        amount,
+        transactionHash
     }
 })
 
-export const transferNftRequest = () => ({
+export const transferNftRequest = ({ fromPrivateKey, toPublicKey, tokenId }) => ({
     type: TRANSFER_NFT_REQUEST,
     payload: {
-
+        fromPrivateKey,
+        toPublicKey,
+        tokenId
     }
 })
-export const transferNftSuccess = () => ({
+export const transferNftSuccess = ({ fromPrivateKey, toPublicKey, tokenId, transactionHash }) => ({
     type: TRANSFER_NFT_SUCCESS,
     payload: {
-
+        fromPrivateKey,
+        toPublicKey,
+        tokenId,
+        transactionHash
     }
 })
 
-export const transferStatusRequest = () => ({
+export const transferStatusRequest = ({ transactionHash }) => ({
     type: TRANSFER_STATUS_REQUEST,
     payload: {
-
+        transactionHash
     }
 })
-export const transferStatusSuccess = () => ({
+export const transferStatusSuccess = ({ transactionHash }) => ({
     type: TRANSFER_STATUS_SUCCESS,
     payload: {
-
+        transactionHash,
+        status
     }
 })
 
-export const walletBalanceRequest = () => ({
+export const walletBalanceRequest = ({ publicKey }) => ({
     type: WALLET_BALANCE_REQUEST,
     payload: {
-
+        publicKey
     }
 })
-export const walletBalanceSuccess = () => ({
+export const walletBalanceSuccess = ({ publicKey, maticAmount, coinsAmount }) => ({
     type: WALLET_BALANCE_SUCCESS,
     payload: {
-
+        publicKey,
+        maticAmount,
+        coinsAmount
     }
 })
 
-export const walletNftBalanceRequest = () => ({
+export const walletNftBalanceRequest = ({ publicKey }) => ({
     type: WALLET_NFT_BALANCE_REQUEST,
     payload: {
-
+        publicKey
     }
 })
-export const walletNftBalanceSuccess = () => ({
+export const walletNftBalanceSuccess = ({ publicKey, balance }) => ({
     type: WALLET_NFT_BALANCE_SUCCESS,
     payload: {
-
+        publicKey,
+        balance
     }
 })
 
-export const generateNftRequest = () => ({
+export const generateNftRequest = ({ transactionHash }) => ({
     type: GENERATE_NFT_REQUEST,
     payload: {
-
+        transactionHash
     }
 })
-export const generateNftSuccess = () => ({
+export const generateNftSuccess = ({ transactionHash, toPublicKey, uri, nftCount}) => ({
     type: GENERATE_NFT_SUCCESS,
     payload: {
-
+        transactionHash,
+        toPublicKey,
+        uri,
+        nftCount
     }
 })
 
-export const listNftRequest = () => ({
+export const listNftRequest = ({ transactionHash }) => ({
     type: LIST_NFT_REQUEST,
     payload: {
-
+        transactionHash
     }
 })
-export const listNftSuccess = () => ({
+export const listNftSuccess = ({ transactionHash, wallet_id, tokens}) => ({
     type: LIST_NFT_SUCCESS,
     payload: {
-
+        transactionHash,
+        wallet_id,
+        tokens
     }
 })
 
-export const statusNftRequest = () => ({
+export const statusNftRequest = ({ tokenId }) => ({
     type: STATUS_NFT_REQUEST,
     payload: {
-
+        tokenId
     }
 })
-export const statusNftSuccess = () => ({
+export const statusNftSuccess = ({ tokenId, uri, publicKey }) => ({
     type: STATUS_NFT_SUCCESS,
     payload: {
-
+        tokenId,
+        uri,
+        publicKey
     }
 })
 
-export const walletHistoryRequest = () => ({
+export const walletHistoryRequest = ({ publicKey }) => ({
     type: WALLET_HISTORY_REQUEST,
     payload: {
-
+        publicKey
     }
 })
-export const walletHistorySuccess = () => ({
+export const walletHistorySuccess = ({ publicKey, history }) => ({
     type: WALLET_HISTORY_SUCCESS,
     payload: {
-
+        publicKey,
+        history
     }
 })
 
